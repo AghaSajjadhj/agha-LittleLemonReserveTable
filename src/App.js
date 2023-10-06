8a8f1d11d999a99b11546ac85d52166cca52fbe3
AghaSajjadhj <146945655+AghaSajjadhj@users.noreply.github.com>
2023-10-07 02:40:48 +0500

    Add files via upload
    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+// import Navbar from './components/nabar/Navbar';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
+
+
+function App() {
+  return (
+     <>
+    
+    <Main />
+    <Footer />
+    </>
+   
+      );
+}
+
+export default App;
---
+++ src/components/main/Main.js
@@ -0,0 +1,69 @@
+import React from "react";
+import "./main.css";
+import { Container, Row, Col } from "react-grid-system";
+function Main() {
+  return (
+    <Container className="container">
+      {/*This is a Row one */}
+      <Row className="row-one">
+        <Col sm={4}>
+          <img
+            src="images/littlelemon_logo.png"
+            style={{ width: 200, height: 100 }}
+          />
+        </Col>
+        <Col>
+          <ul>
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Menu</a>
+            </li>
+            <li>
+              <a className="nav-reserve">Reservation</a>
+            </li>
+            <li>
+              <a>Order Online</a>
+            </li>
+
+            <li>
+              <a>Contact</a>
+            </li>
+            <li>
+              <a>Login</a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+
+      {/*This is a Row Two */}
+      <Row className="row-two">
+        <Col lg={6}>
+          <h1>
+            Little Lemon <br />
+            Chicago
+          </h1>
+          <p>
+            We are a family owned
+            <br />
+            Mediterraneran resturant, <br />
+            focused on traditional reciepies <br />
+            served with a modern twist.
+          </p>{" "}
+          <br />
+          <button>Reserve a table</button>
+        </Col>
+
+        <Col lg={6}>
+          <img src="images\dish.jpg" />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Main;
